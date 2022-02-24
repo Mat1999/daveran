bc131100c7dcc5de0b8de09a2bb4737871ae0ce9
Veres Dávid <20david08@gmail.com>
2022-02-24 18:06:32 +0100

    fix

--- test/lib/home.dart
+++ test/lib/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -10,64 +11,62 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _loading = true;
-  File _image;
-  List _output;
+  File? _image;
+  List? _output;
   final picker = ImagePicker();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    loadModel().then((value){
-      setState(() {
-        
-      });
+    loadModel().then((value) {
+      setState(() {});
     });
   }
 
-  detectImage(File image) async{
+  detectImage(File image) async {
     var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 2,
-      threshold: 0.6,
-      imageMean: 127.5,
-      imageStd: 127.5
-      );
-      setState(() {
-        _output = output;
-        _loading = false;
-      });
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
   }
-  
-  loadModel() async{
-    await Tflite.loadModel(model: 'Assets/model_unquant.tflite',labels: 'Assets/labels.txt');
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'Assets/model_unquant.tflite', labels: 'Assets/labels.txt');
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
   }
 
-  pickImage()async{
+  pickImage() async {
     var image = await picker.getImage(source: ImageSource.camera);
-      if(image == null) return null;
+    if (image == null) return null;
 
+    setState(() {
+      _image = File(image.path);
+    });
 
-      setState(() {
-        _image = File(image.path);
-      });
-
-      detectImage(_image);
+    detectImage(_image!);
   }
-  pickGalleryImg()async{
+
+  pickGalleryImg() async {
     var image = await picker.getImage(source: ImageSource.gallery);
-      if(image == null) return null;
+    if (image == null) return null;
 
+    setState(() {
+      _image = File(image.path);
+    });
 
-      setState(() {
-        _image = File(image.path);
-      });
-
-      detectImage(_image);
+    detectImage(_image!);
   }
 
   @override
@@ -76,56 +75,101 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.blue,
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(height: 50),
-          Text('coding cafe', style: TextStyle(color: Colors.white,fontSize: 20)),
-          SizedBox(height: 5),
-          Text('Cat dog',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 30)),
-          SizedBox(height: 50),
-          Center(child: _loading ? Container(
-            width: 400,
-            child: Column(children: <Widget>[
-              Image.asset('Assets/ye.png'),
-              SizedBox(height: 50)
-            ],),
-          ): Container(),
-          ),
-          Container(width: MediaQuery.of(context).size.width,
-          child: Column(children: <Widget>[
-            GestureDetector(onTap: (){},
-            child: Container(
-              width: MediaQuery.of(context).size.width-250,
-              alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(horizontal: 2,vertical: 18),
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(6),
-                ),
-                child: Text(
-                  'Capture a photo',
-                  style: TextStyle(color: Colors.white,fontSize: 16),
-                ),
-              ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 50),
+            Text('coding cafe',
+                style: TextStyle(color: Colors.white, fontSize: 20)),
+            SizedBox(height: 5),
+            Text('Cat dog',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30)),
+            SizedBox(height: 50),
+            Center(
+              child: _loading
+                  ? Container(
+                      width: 400,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('Assets/ye.png'),
+                          SizedBox(height: 50)
+                        ],
+                      ),
+                    )
+                  : Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 50,
+                            child: Image.file(_image!),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          _output != null
+                              ? Text(
+                                  '${_output![0]['label']}',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 15),
+                                )
+                              : Container(),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
             ),
-          SizedBox(height: 10),
-          GestureDetector(onTap: (){},
-            child: Container(
-              width: MediaQuery.of(context).size.width-250,
-              alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 18),
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(6),
-                ),
-                child: Text(
-                  'Select a photo',
-                  style: TextStyle(color: Colors.white,fontSize: 15),
-                ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      pickImage();
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 250,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 2, vertical: 18),
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        'Capture a photo',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      pickGalleryImg();
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 250,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        'Select a photo',
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],),)
-        ],
+            )
+          ],
         ),
       ),
     );
